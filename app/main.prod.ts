f62fd202b264d33463f380986c47f92835030346
Sam Verschueren <sam.verschueren@gmail.com>
2016-11-14 14:27:56 +0100

    fix aot build

--- app/main.prod.ts
+++ app/main.prod.ts
@@ -1,5 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 
 import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
 
-platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory);
+enableProdMode();
+
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
